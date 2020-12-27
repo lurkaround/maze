dda25d36ce5a0d30868fc3ebac0807f14adf133f
lurkaround <rhelon@gmail.com>
2020-12-27 10:19:37 +0100

    Generated grid

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -27,3 +28,43 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// Maze Generation
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+console.log(startRow, startColumn);
+
+const stepThroughGridCells = (row, column) => {
+  // If visited the cell at [row/column], return
+  grid[row][column] = true;
+  // Mark cell as visited
+
+  // Assemble randomly-ordered list of neighbors
+
+  // for each neighbor....
+
+  // See if neighbor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // remove a wall from either horizontals or verticals
+
+  // Visit next cell
+};
+
+stepThroughGridCells(startRow, startColumn);
+console.log(grid);
